d387d2e097f00b406fc06911cb746c4c4fcda635
maxcadams <max.c.adams@gmail.com>
2022-04-25 18:22:31 -0400

    Added javascript code. Changes to nav bar too.

---
+++ interactions.js
@@ -0,0 +1,24 @@
+// nav bar stuff
+/*$(".element").hover( function() {
+    $(this).animate({width: "200px"}, 5000); } );*/
+$("#home").click (function() {
+    window.location.href = "index.html";
+})
+
+$("#experience").click (function () {
+    window.location.href = "#exp";
+})
+
+$("#projects").click (function () {
+    window.location.href = "#proj";
+})
+
+$("#coursework").click (function () {
+    window.location.href = "#cw";
+})
+
+$("#deez").click (function () {
+    window.location.href = "https://github.com/maxcadams";
+})
+
+
