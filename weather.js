40ecf891738317c468e8bdb61e6e39928704da74
maxcadams <max.c.adams@gmail.com>
2022-05-15 13:20:48 -0400

    fetch api stuff

--- weather.js
+++ weather.js
@@ -1,1 +1,6 @@
 /*      Weather API and search bar     */
+
+fetch('https://www.metaweather.com/api/location/search/?query=london', {
+    //mode: 'no-cors'
+}).then(res => console.log(res))
+  .catch(error => console.log('ERROR'))
